1aaee380319c9daf64e3a35040352a974067eda5
= <patrikrose88@gmail.com>
2017-03-23 22:06:41 -0700

    Started part two of tutorial

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -3,54 +3,73 @@ import logo from './logo.svg';
 import './App.css';
 var ReactDOM = require('react-dom');
 
-var DATA = {    
-    name: 'Patrik Rose',
-    imgURL: 'http://lorempixel.com/200/200/',
-    hobbyList: ['coding', 'boxing', 'hiking', 'tennis']
-}
-
 var App = React.createClass({
+    getInitialState: function(){
+        return {
+            searchResults: []
+        }
+    },
+
+    showResults: function(response){
+        this.setState({
+            searchResults: response.results
+        })
+    },
+
+    search: function(URL){
+        $.ajax({
+            type: "GET",
+            dataType: 'jsonp',
+            url: URL,
+            success: function(response){
+                this.showResults(response);
+            }.bind(this)
+        });
+    },
+
     render: function(){
         return (
             <div>
-                <Profile 
-                    name={this.props.profileData.name}
-                    imgURL={this.props.profileData.imgURL}
-                />
-                <Hobbies 
-                    hobbyList={this.props.profileData.hobbyList}
-                /> 
+                <SearchBox />
+                <Results />
+            </div>
+        );
+    },
+
+    componentDidMount(){
+        this.search('https://itunes.apple.com/search?term=fun');
+    }
+});
+
+var SearchBox = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <input type="text" />
+                <select>
+                    <option value="software">Apps</option>
+                    <option value="movie">Films</option>
+                </select>
+                <input type="submit" />
             </div>
         );
     }
 });
 
-var Profile = React.createClass({
-    render:function(){
-        return (
-            <div>
-            <h3>{this.props.name}</h3>
-            <img src={this.props.imgURL} />
-            </div>
-        );
-    }
-});
-
-var Hobbies = React.createClass({
-    render:function(){
-        var hobbies = this.props.hobbyList.map(function(hobby,index){
-            return (<li key={index}>{hobby}</li>);
-        });
-
-        return (
-            <div>
-            <h3>My hobbies:</h3>
+var Results = React.createClass({
+    render: function(){
+        return(
             <ul>
-                {hobbies}
-            </ul>
-            </div>
+                <ResultItem />
+            </ul>           
         );
     }
 });
 
-ReactDOM.render(<App profileData={DATA} />, document.getElementById('content'));
+var ResultItem = React.createClass({
+    render: function(){
+        return <li>a search result item</li>;
+    }
+});
+
+ReactDOM.render(<App />,  document.getElementById("content"));
